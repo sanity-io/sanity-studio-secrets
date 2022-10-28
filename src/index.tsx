e0b4e59ea64de8b993cb4272d064a382341f6c76
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-10-28 13:07:39 +0200

    feat: initial v3 version

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { useSecrets } from './useSecrets';
-import { SettingsView } from './Settings';
+import {useSecrets} from './useSecrets'
+import {SettingsView, SettingsViewProps} from './Settings'
 
-export { useSecrets, SettingsView };
+export {useSecrets, SettingsView, type SettingsViewProps}
