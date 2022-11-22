a21a528f6ef72e605c0323e0e965ce34f1f2bcb1
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-11-22 16:35:54 +0100

    chore: show studio-v2 in build task

--- src/3rdparty-typings/sanity-parts.d.ts
+++ src/3rdparty-typings/sanity-parts.d.ts
@@ -1,5 +1,5 @@
 declare module 'part:@sanity/base/client' {
-  import { SanityClient } from '@sanity/client'
-  const shim: SanityClient
-  export default shim
+  import { SanityClient } from '@sanity/client';
+  const shim: SanityClient;
+  export default shim;
 }
